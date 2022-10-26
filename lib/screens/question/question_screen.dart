9688c7cfbf0306c01a52e8e01f6080219c10f381
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-10-26 11:19:37 +0100

    made first custom paint painter ui

--- lib/screens/question/question_screen.dart
+++ lib/screens/question/question_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/common/background_decoration.dart';
+
 class QuestionScreen extends StatelessWidget {
   const QuestionScreen({Key? key}) : super(key: key);
   static const routeName = '/questionScreen';
@@ -7,10 +9,8 @@ class QuestionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: const Text("Question Screen"),
-      ),
-    );
+        body: BackgroundDecoration(
+      child: Center(child: Text("Hello")),
+    ));
   }
 }
---
+++ lib/widgets/common/background_decoration.dart
@@ -0,0 +1,54 @@
+import 'package:appkwiz/configs/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class BackgroundDecoration extends StatelessWidget {
+  const BackgroundDecoration({
+    Key? key,
+    required this.child,
+    this.showGradient = false,
+  }) : super(key: key);
+  final Widget child;
+  final bool showGradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Container(
+            decoration: BoxDecoration(
+              color: showGradient ? null : Theme.of(context).primaryColor,
+              gradient: showGradient ? mainGradient() : null,
+            ),
+            child: CustomPaint(
+              painter: BackgroundPainter(),
+            ),
+          ),
+        ),
+        Positioned(child: child)
+      ],
+    );
+  }
+}
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.white.withOpacity(0.1);
+    //..color means from Paint object you take the color type and assign color to it same with below
+    // Paint paint1 = Paint();
+    // paint1.color = Colors.white;
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width * 0.2, 0);
+    path.lineTo(0, size.height * 0.4);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(BackgroundPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(BackgroundPainter oldDelegate) => false;
+}
