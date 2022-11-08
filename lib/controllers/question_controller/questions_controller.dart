75863bcac4f07fc07d6d8063ec65d3f835780fb9
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-11-08 01:37:32 +0100

    minor fix

--- lib/controllers/question_controller/questions_controller.dart
+++ lib/controllers/question_controller/questions_controller.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 
-import 'package:appkwiz/controllers/auth_controller.dart';
 import 'package:appkwiz/controllers/firebase_ref/loading_status.dart';
 import 'package:appkwiz/controllers/firebase_ref/references.dart';
-import 'package:appkwiz/controllers/question_controller/question_paper_controller.dart';
 import 'package:appkwiz/models/question_paper_model.dart';
 import 'package:appkwiz/screens/home/home_screen.dart';
 import 'package:appkwiz/screens/question/result_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
